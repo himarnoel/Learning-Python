f9bad15956118a0eb6f76f093ad96dcd1cdf3663
himarnoel <olaimarnoel@gmail.com>
2023-03-09 22:44:47 +0100

    working with built in module

--- start.py
+++ start.py
@@ -1,11 +1,13 @@
 from helper import *
+import logging
+print(os.name)
 
-user_input = ""
-while user_input != "exit":
-    user_input = input(user_input_message)
-    days_and_unit = user_input.split(":")
-    days_and_unit_dictionary = {
-        "days": days_and_unit[0],
-        "units": days_and_unit[1]
-    }
-    validate_and_execute(days_and_unit_dictionary)
+# user_input = ""
+# while user_input != "exit":
+#     user_input = input(user_input_message)
+#     days_and_unit = user_input.split(":")
+#     days_and_unit_dictionary = {
+#         "days": days_and_unit[0],
+#         "units": days_and_unit[1]
+#     }
+#     validate_and_execute(days_and_unit_dictionary)
