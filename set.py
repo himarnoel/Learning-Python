e26d517a9e8b8496c6f9957e951887db451ade86
himarnoel <olaimarnoel@gmail.com>
2023-03-09 21:34:25 +0100

    working with sets

--- set.py
+++ set.py
@@ -1,1 +1,15 @@
-{"January","February", "Mar"}
+my_set = {"January", "February", "March"}
+for element in my_set:
+    print(element)
+my_set.add("April")
+print(my_set)
+
+# Set does not allow duplicate values
+# It is unordered
+
+my_set.remove("January") 
+print(my_set)
+
+my_list = ["January", "February", "March", "January"]
+my_list.remove("January")
+print(my_list)
